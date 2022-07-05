70127a77b40d8e065b3488c8ea09b90696b7f3ee
Hamaguri-0414 <sato@sec.inf.shizuoka.ac.jp>
2022-07-05 19:24:09 +0900

    change input type

--- QRgen.py
+++ QRgen.py
@@ -3,11 +3,12 @@ import qrcode
 import hashlib
 
 args = sys.argv
-if len(args) != 1:
+if len(args) != 2:
     print("Usage: python QRgen.py [answer]")
 
-ans_sha256 = hashlib.sha256(args[0].encode()).hexdigest()
+ans_sha256 = hashlib.sha256(args[1].encode()).hexdigest()
 URL = "https://hamaguri-0414.github.io/check-answer?answer=" + ans_sha256
 print(URL)
+
 qr = qrcode.make(URL)
 qr.save("QRcode.png")
